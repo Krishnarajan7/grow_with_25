5146722f44c2852ab625fd86de8e305ea34f291d
Krishnarajan <krishh.v777@gmail.com>
2025-07-21 21:43:19 +0530

    fix: On scroll animations

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 pt-20">
+      <main className="flex-1 pt-24">
         {children}
       </main>
       <Footer />
